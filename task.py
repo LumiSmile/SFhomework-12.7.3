3110ff914243c002f0a76fc6d21f78543ebc5f2b
Natalia Ivanova <nataliaivanova@MacBook-Air-Natalia-2.local>
2023-01-15 12:15:47 +0300

    Initial commit

---
+++ task.py
@@ -0,0 +1,10 @@
+money = int(input("Введите сумму: "))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+deposit.append(money*5.6/100)
+deposit.append(money*5.9/100)
+deposit.append(money*4.28/100)
+deposit.append(money*4.0/100)
+
+print(deposit)
+print("Максимальная сумма, которую вы можете заработать:", max(deposit))
